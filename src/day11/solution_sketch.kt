f51292289963ad1baf293f278c90c3cddd5de1e9
wookieJ <lukaszjedryczka.biuro@gmail.com>
2021-12-14 22:03:04 +0100

    Day 11

---
+++ src/day11/solution_sketch.kt
@@ -0,0 +1,83 @@
+package day11
+
+import checkSolution
+
+fun main() {
+    fun cretePowerMap(input: String): MutableMap<Pair<Int, Int>, Int> {
+        val heightMap = mutableMapOf<Pair<Int, Int>, Int>()
+        input.lines().forEachIndexed { row, line ->
+            line.forEachIndexed { col, it -> heightMap[Pair(col, row)] = it.toString().toInt() }
+        }
+        return heightMap
+    }
+
+    fun flashPoint(point: Pair<Int, Int>, energies: MutableMap<Pair<Int, Int>, Int>): Pair<MutableMap<Pair<Int, Int>, Int>, Int> {
+        var flashesCopy = 0
+        var energiesCopy = HashMap(energies).toMutableMap()
+        val neighbours = point.adjacent(10, 10)
+        neighbours.forEach { neighbour ->
+            if (energiesCopy[neighbour]!! in 1..9) {
+                energiesCopy[neighbour] = energiesCopy[neighbour]!! + 1
+                if (energiesCopy[neighbour]!! > 9) {
+                    val newEnergies = flashPoint(neighbour, energiesCopy)
+                    energiesCopy = newEnergies.first
+                    flashesCopy += newEnergies.second
+                }
+            }
+        }
+        energiesCopy[point] = 0
+        flashesCopy++
+        return Pair(energiesCopy, flashesCopy)
+    }
+
+    fun part1(input: String): Int {
+        var energies = cretePowerMap(input)
+        var flashes = 0
+        repeat(100) { i ->
+            energies = energies.mapValues { it.value + 1 }.toMutableMap() // step 1
+            energies.forEach { (point, energy) ->
+                if (energy > 9) {
+                    val newEnergies = flashPoint(point, energies)
+                    energies = newEnergies.first
+                    flashes += newEnergies.second
+                }
+            }
+        }
+        return flashes
+    }
+
+    fun part2(input: String): Int {
+        var energies = cretePowerMap(input)
+        var i = 0
+        while (i > -1) {
+            if (energies.all { it.value == 0 }) {
+                return i
+            }
+            energies = energies.mapValues { it.value + 1 }.toMutableMap() // step 1
+            energies.forEach { (point, energy) ->
+                if (energy > 9) {
+                    val newEnergies = flashPoint(point, energies)
+                    energies = newEnergies.first
+                }
+            }
+            i++
+        }
+        return -1
+    }
+
+    checkSolution(11, ::part1, ::part2, 1656, 195)
+}
+
+private fun Pair<Int, Int>.adjacent(rowsNumber: Int, colsNumber: Int): List<Pair<Int, Int>> {
+    return listOf(
+        Pair(this.first - 1, this.second - 1),
+        Pair(this.first, this.second - 1),
+        Pair(this.first + 1, this.second - 1),
+        Pair(this.first - 1, this.second),
+        Pair(this.first + 1, this.second),
+        Pair(this.first - 1, this.second + 1),
+        Pair(this.first, this.second + 1),
+        Pair(this.first + 1, this.second + 1),
+    ).filter { it.first in 0 until colsNumber && it.second in 0 until rowsNumber }
+}
+
